105fc9202dac5f1a1b86bcfda0d3fcf558c702ad
Ziyad <ziyadsbeih0.0@gmail.com>
2023-10-05 22:57:05 -0400

    added some routes

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,51 @@ const port = 5000;
 // Serve static files from the build directory
 app.use(express.static(path.join(__dirname, '/build')));
 
-// Define a route to handle all other requests and serve the React app
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Route to homepage
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/build', '/index.html'));
+});
+
+// Api route, get list of all cities
+app.get('/api/cities', (req, res) => {
+
+  testCities = [{id: 1, name: 'Clevland'}, {id: 2, name: 'New York'}, {id: 3, name: 'Seattle'}]
+
+  /* CALL TO DATABASE */
+
+  res.json(testCities)
+});
+
+// Api route, get city info given id
+app.get('/api/cities/:id', (req, res) => {
+
+  const {id} = req.params;
+  testCities = [{id: 1, name: 'Cleveland', population: '367,991', avg_salary: 21507}, {id: 2, name: 'New York'}, {id: 3, name: 'Seattle'}]
+
+  /* CALL TO DATABASE*/
+
+  const city = testCities.find(c => c.id == id);
+
+  res.json(city);
+});
+
+// Api route, search for cities with given search criteria
+app.post('/api/search', (req, res) => {
+  
+  const searchCriteria = req.body;
+
+  /* ADD SEARCH FUCNTION CALL */
+
+  res.json(searchCriteria);
+});
+
+// Global Error-Handeling MiddleWare. All uncaught error at api endpoints end up here
+app.use((err, req, res, next) => {
+  console.error('Error:', err);
+  res.status(500).json({ error: 'Internal Server Error' });
 });
 
 // Start the Express server
