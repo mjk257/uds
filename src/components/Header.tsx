8b3e90bd70f4cb00097232f8a3777a76d949fcaa
Peter Schlueter <petder02@gmail.com>
2023-10-29 16:51:54 -0400

    Added items to header
    Will add pop-ups with basic stuff soon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,27 @@
 import React from "react";
-import {AppBar, MenuItem, Toolbar, Typography} from "@mui/material";
+import {AppBar, Box, Button, Grid, MenuItem, Toolbar, Typography} from "@mui/material";
 
 const CityPreferencesForm = () => {
+
+    const navItems = ['About Us', 'Credits', 'Help'];
+    const headerSx = { display: { xs: 'none', sm: 'block' } };
+
     return (
         <div>
             <AppBar position="static" className='header-bar'>
                 <Toolbar>
-                    <Typography variant='h4' component='div' className='header-bar-text' align='left'>
+                    <Typography variant='h4' component='div'
+                                className='header-bar-text' align='left'
+                                sx={ headerSx }>
                         UDS: Urban Discovery System
                     </Typography>
+                    <Box sx={ headerSx }>
+                        {navItems.map((item) => (
+                            <Button key={item} color='inherit'>
+                                {item}
+                            </Button>
+                        ))}
+                    </Box>
                 </Toolbar>
             </AppBar>
         </div>
