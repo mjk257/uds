5ed0642b74f72dd3e4aacc8bf359018f548d9f1f
mjk257 <mjk257@case.edu>
2023-11-03 11:30:03 -0400

    Updated climate script and added full ingestion

--- python/climate.py
+++ python/climate.py
@@ -1,11 +1,50 @@
 import requests
 import os
+import numpy
+import pandas as pd
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
+# function for getting the bounding box from coordinates
+def get_gps_bounding_box(latitude, longitude, deg_lat, deg_lon):
+
+    if latitude >= 0:
+        if latitude + deg_lat >= 90.0:
+            n = 90.0 * numpy.sign(latitude)
+            s = latitude - deg_lat
+        else:   
+            n = latitude + deg_lat
+            s = latitude - deg_lat
+
+    else:
+        if abs(latitude) + deg_lat >= 90.0:
+            n = 90.0 * numpy.sign(latitude)
+            s = (abs(latitude) - deg_lat) * numpy.sign(latitude)
+        else:   
+            n = latitude + deg_lat
+            s = latitude - deg_lat
+
+    if longitude >= 0:
+        if longitude + deg_lon >= 180.0:
+            w = 180.0 * numpy.sign(longitude)
+            e = longitude - deg_lon
+        else:   
+            w = longitude + deg_lon
+            e = longitude - deg_lon
+
+    else:
+        if abs(longitude) + deg_lon >= 180.0:
+            w = 180.0 * numpy.sign(longitude)
+            e = (abs(longitude) - deg_lon) * numpy.sign(longitude)
+        else:   
+            w = longitude + deg_lon
+            e = longitude - deg_lon
+            
+    return ",".join([str(n), str(w), str(s), str(e)])
+
 load_dotenv("../.env")
 
-# API url
+# Climate Zone API url
 api = "http://climateapi.scottpinkelman.com/api/v1/location/"
 
 # DB connection string
@@ -15,9 +54,78 @@ db_url = os.getenv("MONGODB_URL")
 client = MongoClient(db_url)
 cities = client['uds']['cities']
 
+# Arrays to define summer and winter months
+summer = [5, 6, 7]
+winter = [0, 1, 11]
+
 # Call API and add climate zone and zone description
 for city in cities.find():
-    response = requests.get(api + str(city['latitude']) + "/" + str(city['longitude'])).json()['return_values'][0]
+    zone_response = requests.get(api + str(city['latitude']) + "/" + str(city['longitude'])).json()['return_values'][0]
+
+    lat = city['latitude']
+    lon = city['longitude']
+
+    stations_list = []
+
+    # API to get weather stations for coordiantes
+    params = {
+        "dataset": "global-summary-of-the-month",
+        "bbox": get_gps_bounding_box(lat, lon, 0.5, 0.5),
+        "dataType": ",".join(["TAVG", "PRCP", "SNOW"])
+    }
+    url = "https://www.ncei.noaa.gov/access/services/search/v1/data"
+
+    for i in range(3):
+        try:
+            response = requests.get(url, params=params)
+            if response.status_code == 200:
+                break
+        except:
+            pass 
+
+    # Add stations to list
+    for result in response.json()['results']:
+        for station in result['stations']:
+            stations_list.append(station["id"])
+
+    # API to get monthly data from the stations
+    url = "https://www.ncei.noaa.gov/access/services/data/v1/"
+    params = {
+        "dataset": "global-summary-of-the-month",
+        "stations": ",".join(stations_list),
+        "startDate": "1975-01-01",
+        "endDate": "2022-01-01",
+        "bbox": get_gps_bounding_box(lat, lon, 0.5, 0.5),
+        "dataType": ",".join(["TAVG", "PRCP", "SNOW"]),
+        "includeAttributes": "false",
+        "format": "json",
+        "units": "standard"
+    }
+
+    for i in range(3):
+        try:
+            response = requests.get(url, params=params)
+            if response.status_code == 200:
+                break
+        except:
+            pass 
+
+    # Add monthly data to dataframe and clean
+    df = pd.DataFrame(response.json())
+    df = df[["TAVG", "DATE", "PRCP", "SNOW"]]
+    df = df.dropna()
+    df['TAVG'] = df['TAVG'].astype(float)
+    df['PRCP'] = df['PRCP'].astype(float)
+    df['SNOW'] = df['SNOW'].astype(float)
+    df['DATE'] = df['DATE'].str.replace(r'^[^-]*-*', '', regex=True)
+    df.reset_index(inplace = True, drop = True)
+
+    # Update database entries with climate data
     query = { "_id": city["_id"] }
-    values = { "$set": { "climate_zone":  response['koppen_geiger_zone'], "zone_description": response['zone_description']} }
+    values = { "$set": { "summer_temp":  int(df.groupby("DATE")['TAVG'].mean().take(summer, axis=0).mean()), 
+                        "winter_temp": int(df.groupby("DATE")['TAVG'].mean().take(winter, axis=0).mean()), 
+                        "annual_precipitation": int(df.groupby("DATE")['PRCP'].mean().sum()),
+                       "annual_snow": int(df.groupby("DATE")['SNOW'].mean().sum()),
+                       "climate_zone":  zone_response['koppen_geiger_zone'], 
+                       "zone_description": zone_response['zone_description']} }
     cities.update_one(query, values)
---
+++ python/full_ingestion.py
@@ -0,0 +1,26 @@
+import subprocess
+
+print("Running full data ingestion script. This script will wipe the database selected in the .env file and will take some time to run.")
+while True:
+    if input('Do you wish to continue? [y/n] ') != 'y':
+        break
+    print("Installing dependencies...")
+    subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
+    print("Dependencies installed. Running cities script...")
+    subprocess.run(["python", "cities.py"])
+    print("Cities script completed. Running RPP script...")
+    subprocess.run(["python", "rpp.py"])
+    print("RPP script completed. Running outdoor score script...")
+    subprocess.run(["python", "outdoor_score.py"])
+    print("Outdoor score script completed. Running median age script...")
+    subprocess.run(["python", "median_age.py"])
+    print("Median age script completed. Running partisan lean script...")
+    subprocess.run(["python", "partisan_lean.py"])
+    print("Partisan lean script completed. Running climate script...")
+    subprocess.run(["python", "climate.py"])
+    print("Climate script completed. Running AI description script...")
+    # subprocess.run(["python", "ai_description.py"])
+    print("AI description script completed. Running job codes script...")
+    subprocess.run(["python", "job_codes.py"])
+    print("Job codes script completed. All scripts have finished.")
+    break
