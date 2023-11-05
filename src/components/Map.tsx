98af938d8c5ed102dffc0f0a924fd37ee2a82b5b
francismungai <fwm25@case.edu>
2023-11-05 10:59:49 -0500

    Added marker clusters
    Now markers show up us clusters and separate into individual markers as you zoom in
    I removed the popups.

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useMemo } from "react";
 import mapboxgl from "mapbox-gl";
-import "../styles/index.scss"; // Import the CSS file
+import "mapbox-gl/dist/mapbox-gl.css"; // Import Mapbox CSS
+import supercluster from "supercluster"; // Import the supercluster library
+import "../styles/index.scss";
 
 interface City {
   name: string;
@@ -16,72 +18,178 @@ interface MapProps {
 }
 
 const Map: React.FC<MapProps> = ({ cities, onMarkerClick }) => {
-  const US_BOUNDS = {
-    west: -125.0, // The westernmost point of the U.S.
-    south: 24.396308, // The southernmost point of the U.S.
-    east: -66.93457, // The easternmost point of the U.S.
-    north: 49.345786, // The northernmost point of the U.S.
-  };
+  const mapContainer = useRef<HTMLDivElement | null>(null);
+  const map = useRef<mapboxgl.Map | null>(null);
+  const cluster = useMemo(() => new supercluster(), []);
 
   useEffect(() => {
     mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN as string;
 
-    const map = new mapboxgl.Map({
-      container: "map",
+    // Initialize the map
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current as HTMLDivElement, // Update the container property here
       style: "mapbox://styles/fwm25/clnxrvfh9007y01qshgob0qej",
-      center: [-98.583333, 39.833333], // Center the map on the US
-      zoom: 3, // Adjust the zoom level as needed
+      center: [-98.583333, 39.833333],
+      zoom: 3,
       maxBounds: [
-        [US_BOUNDS.west, US_BOUNDS.south], // Southwestern bound
-        [US_BOUNDS.east, US_BOUNDS.north], // Northeastern bound
+        [-125.0, 24.396308], // Southwestern bound
+        [-66.93457, 49.345786], // Northeastern bound
       ],
     });
 
-    // Add markers for each city
-    cities.forEach((city) => {
-      const marker = new mapboxgl.Marker()
-        .setLngLat([city.longitude, city.latitude])
-        .setPopup(
-          new mapboxgl.Popup().setHTML(
-            `<h3>${city.name}, ${city.state}</h3><p>Population: ${city.population}</p>`
-          )
-        )
-        .addTo(map);
+    // Add zoom and rotation controls to the map.
+    map.current.addControl(new mapboxgl.NavigationControl(), "top-right");
 
-      // Set the city's name as the marker's unique identifier
-      marker.getElement().setAttribute("data-city-name", city.name);
+    map.current.on("load", () => {
+      // Convert the cities to GeoJSON features
+      const points = cities.map((city) => ({
+        type: "Feature" as const,
+        properties: {
+          cluster: false,
+          cityId: city.name,
+          category: city.state,
+        },
+        geometry: {
+          type: "Point" as const,
+          coordinates: [city.longitude, city.latitude],
+        },
+      }));
 
-      // Create a label for the marker
-      const label = document.createElement("div") as HTMLElement;
-      label.className = "marker-label";
-      label.innerHTML = city.name;
+      // Load the points into the Supercluster
+      cluster.load(points);
 
-      // Add the label to the marker
-      marker.getElement().appendChild(label);
+      // Get all clusters
+      const bbox: [number, number, number, number] = [-180, -85, 180, 85];
+      const zoom = map.current?.getZoom() || 0;
+      const clusters = cluster.getClusters(bbox, zoom);
 
-      // Add a click event listener to the marker
-      marker.getElement().addEventListener("click", (e) => {
-        const cityName = marker.getElement().getAttribute("data-city-name");
+      // Add clusters to the map
+      map.current?.addSource("clusters", {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: clusters,
+        },
+      });
+
+      // Add cluster and uncluster layers
+      map.current?.addLayer({
+        id: "clusters",
+        type: "circle",
+        source: "clusters",
+        filter: ["has", "point_count"],
+        paint: {
+          "circle-color": "#007cbf",
+          "circle-radius": 18,
+          "circle-stroke-width": 2,
+          "circle-stroke-color": "#fff",
+        },
+        layout: {
+          visibility: "visible", // Make sure the cluster layer is visible
+        },
+      });
+
+      map.current?.addLayer({
+        id: "cluster-count",
+        type: "symbol",
+        source: "clusters",
+        filter: ["has", "point_count"],
+        layout: {
+          "text-field": "{point_count_abbreviated}",
+          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+          "text-size": 12,
+          visibility: "visible",
+        },
+      });
+
+      map.current?.addLayer({
+        id: "unclustered-point",
+        type: "circle",
+        source: "clusters",
+        filter: ["!", ["has", "point_count"]],
+        paint: {
+          "circle-color": "#007cbf",
+          "circle-radius": 10,
+          "circle-stroke-width": 2,
+          "circle-stroke-color": "#fff",
+        },
+        layout: {
+          visibility: "visible", // Make sure the cluster layer is visible
+        },
+      });
+
+      ["clusters", "unclustered-point"].forEach((layer) => {
+        map.current?.on("mouseenter", layer, () => {
+          if (map.current) {
+            map.current.getCanvas().style.cursor = "pointer";
+          }
+        });
+
+        map.current?.on("mouseleave", layer, () => {
+          if (map.current) {
+            map.current.getCanvas().style.cursor = "";
+          }
+        });
+      });
+
+      map.current?.on("zoomend", () => {
+        const zoom = map.current?.getZoom() || 0;
+        const clusters = cluster.getClusters(bbox, zoom);
+
+        if (map.current?.getSource("clusters")) {
+          const source = map.current.getSource(
+            "clusters"
+          ) as mapboxgl.GeoJSONSource;
+          source.setData({
+            type: "FeatureCollection",
+            features: clusters,
+          });
+        }
+      });
+
+      // Handle marker click
+
+      map.current?.on("click", "clusters", (e) => {
+        const features = map.current?.queryRenderedFeatures(e.point, {
+          layers: ["clusters"],
+        });
+        const clusterId = features?.[0]?.properties?.cluster_id;
+        const clickedCluster = clusterId
+          ? cluster.getLeaves(clusterId, Infinity)
+          : null;
+
+        if (clickedCluster) {
+          const coordinates = clickedCluster[0].geometry.coordinates as [
+            number,
+            number
+          ];
+
+          map.current?.easeTo({
+            center: coordinates,
+            zoom: map.current?.getZoom() + 1,
+          });
+        }
+      });
+
+      map.current?.on("click", "unclustered-point", (e) => {
+        const cityName = e.features?.[0]?.properties?.name;
         if (cityName) {
-          onMarkerClick(cityName); // Call the function to open the response card
+          onMarkerClick(cityName);
         }
       });
     });
 
     // Clean up the map on unmount
-    return () => map.remove();
-  }, [
-    US_BOUNDS.east,
-    US_BOUNDS.north,
-    US_BOUNDS.south,
-    US_BOUNDS.west,
-    cities,
-    onMarkerClick,
-  ]);
+    return () => {
+      if (map.current) {
+        map.current.remove();
+      }
+    };
+  }, [cities, cluster, onMarkerClick]);
 
   return (
     <div id="map-container">
-      <div id="map" />
+      <div ref={mapContainer} id="map" className="map-container" />
     </div>
   );
 };
