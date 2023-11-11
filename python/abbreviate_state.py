6dfbe44d4bc83c457ec99c0eac20a679d8ddc963
mjk257 <mjk257@case.edu>
2023-11-11 14:48:47 -0500

    Add crime rate script

--- python/abbreviate_state.py
+++ python/abbreviate_state.py
@@ -55,6 +55,7 @@ us_state_to_abbrev = {
     "Wisconsin": "WI",
     "Wyoming": "WY",
     "District of Columbia": "DC",
+    "District Of Columbia": "DC",
     "American Samoa": "AS",
     "Guam": "GU",
     "Northern Mariana Islands": "MP",
